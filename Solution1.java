33d7016dab4c935c0dc9eacd11bd2e3159ad127d
Santhiya <169180434+Santhiya-sm-15@users.noreply.github.com>
2025-04-14 07:45:09 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int cnt=0;
+        int i,j,k,n=arr.length;
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                for(k=j+1;k<n;k++)
+                {
+                    int x=Math.abs(arr[i]-arr[j]);
+                    int y=Math.abs(arr[j]-arr[k]);
+                    int z=Math.abs(arr[i]-arr[k]);
+                    if(x<=a && y<=b && z<=c)
+                        cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
